53bd5a07339af02fc72b136592a7b5986c3e7b12
Paquita3000 <149542172+Paquita3000@users.noreply.github.com>
2024-05-27 11:41:53 -0600

    Add files via upload

--- funciones/funciones1.js
+++ funciones/funciones1.js
@@ -1,3 +1,26 @@
+function actualizarDatos() {
+    let codigo = document.getElementById('codigo').value;
+    let huella = document.getElementById('huella').value;
+    let tarjeta = document.getElementById('tarjeta').value;
+    let facial = document.getElementById('facial').value;
+  
+    $.ajax({
+      url:'funciones/updateDatos.php',
+      type: 'POST',
+      dataType: 'text',
+      data: 
+      'codigo=' + codigo + '&huella=' + huella + '&tarjeta=' + tarjeta + '&facial=' + facial,
+      
+      success: function(res) {
+        alert(res);
+
+      },
+      error: function() {
+        alert('Error: archivo no encontrado');
+      }
+    });
+}
+  
 function enviarDatos() {
     let codigo = document.getElementById('codigo').value;
     let huella = document.getElementById('huella').value;
