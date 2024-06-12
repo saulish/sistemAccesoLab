c94aee3f566428cbd2e6c327ba736f6e829fac8a
Paquita3000 <149542172+Paquita3000@users.noreply.github.com>
2024-06-12 10:58:39 -0600

    Add files via upload

--- sistemaaccesolab.sql
+++ sistemaaccesolab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-05-2024 a las 18:12:47
+-- Tiempo de generación: 12-06-2024 a las 18:58:15
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -11,63 +11,102 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- Cambiar juego de caracteres
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
 -- Base de datos: `sistemaaccesolab`
+--
 
--- Crear tabla `datos_biometricos`
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos_biometricos`
+--
+
 CREATE TABLE `datos_biometricos` (
   `id` int(11) NOT NULL,
-  `codigo` int(10) NOT NULL,
-  `Dhuella` varchar(100) NOT NULL,
-  `Dfacial` varchar(100) NOT NULL,
-  `DTarjeta` varchar(100) NOT NULL,
-  `turno` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `codigo` int(11) NOT NULL,
+  `Dhuella` varchar(255) NOT NULL,
+  `Dtarjeta` varchar(255) NOT NULL,
+  `Dfacial` varchar(255) NOT NULL,
+  `turno` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Insertar datos en `datos_biometricos`
-INSERT INTO `datos_biometricos` (`id`, `codigo`, `Dhuella`, `Dfacial`, `DTarjeta`,`turno`) VALUES
-(1, 218879131, 'huella1', 'facil1', 'tarjeta1', 1);
+--
+-- Volcado de datos para la tabla `datos_biometricos`
+--
 
--- Crear tabla `usuarios`
+INSERT INTO `datos_biometricos` (`id`, `codigo`, `Dhuella`, `Dtarjeta`, `Dfacial`, `turno`) VALUES
+(1, 218715581, 'huella_juan', 'tarjeta_juan', 'facial_juan', 1),
+(2, 218715582, 'huella_maria', 'tarjeta_maria', 'facial_maria', 2),
+(3, 218715583, 'huella_carlos', 'tarjeta_carlos', 'facial_carlos', 3),
+(4, 218715584, 'huella_ana', 'tarjeta_ana', 'facial_ana', 4),
+(5, 218715585, 'huella_luis', 'tarjeta_luis', 'facial_luis', 5),
+(8, 1, 'pabloH', 'pabloT', 'pabloC', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `codigo` int(10) NOT NULL,
   `telefono` int(10) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `activo` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `activo` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Insertar datos en `usuarios`
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`id`, `codigo`, `telefono`, `nombre`, `activo`) VALUES
-(1, 218879131, 2147483647, 'Saul', 1);
+(1, 218879131, 2147483647, 'Saul', 1),
+(5, 218715589, 333842711, 'Oscar Martinez', 1),
+(11, 218715581, 555, 'Juan Pérez', 1),
+(12, 218715582, 555, 'María López', 1),
+(13, 218715583, 555, 'Carlos García', 0),
+(14, 218715584, 555, 'Ana Fernández', 1),
+(15, 218715585, 555, 'Luis Martínez', 0),
+(17, 1, 1, 'aaaaa', 1);
 
--- Crear tabla `turno_personalizado`
-CREATE TABLE `turno_personalizado` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `day_of_week` varchar(10) NOT NULL,
-  `shift` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `usuarios`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Índices para tablas volcadas
+--
 
--- Establecer AUTO_INCREMENT para las tablas
+--
+-- Indices de la tabla `datos_biometricos`
+--
 ALTER TABLE `datos_biometricos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `turno_personalizado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `datos_biometricos`
+--
+ALTER TABLE `datos_biometricos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
